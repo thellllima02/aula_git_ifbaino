3e2a759c25a2e78710310db25dd225d6d7b79bd8
Flávia Correia <fllaviacorreia@outlook.com>
2024-11-19 10:31:45 -0300

    rm comentarios

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 document.querySelector('.login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio do formulário
-
+    event.preventDefault(); 
+    
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
-    console.log("username",username, "password", password)
+    
     if(username === 'username' && password === 'username'){
         window.location.href = 'home.html';
     }
     else {
         alert('Usuário ou senha não reconhecidos.')
-        // alert(`Bem-vindo, ${username}!`);
-        // Aqui você pode redirecionar o usuário para outra página
     }
 });
